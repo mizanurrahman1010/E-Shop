ac5d2bfea284c2d3f3fa2f2b65ddd152982d21bc
Md. Mizanur Rahman <79052925+mizanurrahmanreyad@users.noreply.github.com>
2021-07-12 23:32:15 +0600

    E-Shop Frontend

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+import{BrowserRouter,Route} from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
+import Protected from './Protected'
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Header />
+      <h1>E Shop React Laravel</h1>
+      <Route path="/login">
+      <Login />
+      </Route>
+      <Route path="/register">
+      <Register />
+      </Route>
+      <Route path="/add">
+        <Protected Cmp={AddProduct}/>
+      {/* <AddProduct /> */}
+      </Route>
+      <Route path="/update">
+      <Protected Cmp={UpdateProduct} />
+      {/* <UpdateProduct /> */}
+      </Route>
+      </BrowserRouter>
+
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Link,useHistory } from 'react-router-dom'
+function Header() {
+    let user= JSON.parse(localStorage.getItem('user-info'))
+    function logOut()
+    {
+        localStorage.clear();
+        // history.pushState('/register');
+
+    }
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">E-Shop</Navbar.Brand>
+                <Nav className="mr-auto navbar_wrapper">
+                    {
+                        localStorage.getItem('user-info') ?
+                            <>
+                                <Link to="/add">Add Product</Link>
+                                <Link to="/update">Update Product</Link>
+                            </>
+                            :
+                            <>
+                                <Link to="/login">Login</Link>
+                                <Link to="/register" > Register</Link >
+                            </>
+                    }
+
+
+
+                </Nav >
+                { localStorage.getItem('user-info')?
+                      <Nav>
+                        <NavDropdown title={user && user.name}>
+                        <NavDropdown.Item onClick={logOut}>Log Out</NavDropdown.Item>
+                        </NavDropdown>
+                      </Nav>
+                :null
+}
+            </Navbar>
+            
+        </div>
+)
+}
+export default Header
